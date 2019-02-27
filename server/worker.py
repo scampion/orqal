6bf4eda5b84b25dfe0a2f5a018a6868c615ffe1e
Sébastien Campion <sebastien.campion@inria.fr>
2019-02-27 15:21:17 +0100

    update

--- server/worker.py
+++ server/worker.py
@@ -8,7 +8,7 @@ dockers = [docker.DockerClient(base_url=h) for h in conf.docker_hosts]
 class Test:
     docker_url = "registry.gitlab.inria.fr/scampion/madlab/test_module"
 
-    def __init__(self, id, client, tag='latest'):
+    def __init__(self, id, input, params, client, tag='latest'):
         client.images.pull(self.docker_url + ':' + tag)
         client.run()
 
