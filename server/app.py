90296ac18bda1e2e6d7f62166b45f4eac3a18d8d
Mathieu SIMON <mathieu.simon@inria.fr>
2019-02-27 16:53:29 +0100

    Link with mongodb

--- server/app.py
+++ server/app.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, abort
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/madlab"
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "MADLAB - WIP"
 
-@app.route('/job', method='POST')
+@app.route('/job', methods=['POST'])
 def create_job():
     data = request.get_json()
     if data is None:
         abort(500)
     app = data.get('app', None)
-    inputFile = data.get('input', None)
+    input_file = data.get('input', None)
     params = data.get('params', None)
-    return "Hello, World!"
-    #return {id, status, metadata, app, input, params, result, logs{stdout, stderr}}
+    _id = mongo.db.jobs.insert({'app': app, 'input': input_file, 'params': params})
+    return str(_id)
 
 @app.route('/job/<id>')
 def get_job(id):
-    return "Hello, World!"
-    #return {id, status, metadata, app, input, params, result, logs{stdout, stderr}}
+    return dumps(mongo.db.jobs.find_one_or_404({'_id': ObjectId(id)}))
 
-@app.route('/jobs/status', method='POST')
+@app.route('/jobs/status', methods=['POST'])
 def get_jobs_status():
     try:
         data = request.get_json()
