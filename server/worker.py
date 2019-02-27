fa85579411a34f186c78e996e714495cc54c0631
Sébastien Campion <sebastien.campion@inria.fr>
2019-02-27 11:23:41 +0100

    update

--- server/worker.py
+++ server/worker.py
@@ -2,13 +2,17 @@ import docker
 
 import conf
 
-dockers = [docker.DockerClient(base_url=h) for h in conf.docker_hosts]
+token = "65sX2-9sSXSp-hs-XeZ8"
+dockers = [docker.client.Client(base_url=h) for h in conf.docker_hosts]
 
 
 class App:
-    docker_url = None
+    docker_url = "registry.gitlab.inria.fr/scampion/madlab/test_module"
 
     def __init__(self):
         for d in dockers:
             d.images.pull(self.docker_url)
 
+
+if __name__ == '__main__':
+    App()
