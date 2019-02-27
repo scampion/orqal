fa9020a3dfaed2c409c91bca1fe9f5e0d38943a4
Mathieu SIMON <mathieu.simon@inria.fr>
2019-02-27 12:02:34 +0100

    First API version

--- server/http.py
+++ server/http.py
@@ -8,6 +8,33 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/job', method='POST')
+def create_job():
+    try:
+        data = request.get_json()
+        if data is None:
+            abort(500)
+        app = data.get('app', None)
+        inputFile = data.get('input', None)
+        params = data.get('params', None)
+    except Exception as e:
+        abort(500)
+    return "Hello, World!"
+    #return {id, status, metadata, app, input, params, result, logs{stdout, stderr}}
+
+@app.route('/job/<id>')
+def get_job(id):
+    return "Hello, World!"
+    #return {id, status, metadata, app, input, params, result, logs{stdout, stderr}}
+
+@app.route('/jobs/status', method='POST')
+def get_jobs_status():
+    try:
+        data = request.get_json()
+    except Exception as e:
+        abort(500)
+    return "Hello, World!"
+    #return {finish:[], error:[],submitted}
 
 if __name__ == '__main__':
     app.run()
