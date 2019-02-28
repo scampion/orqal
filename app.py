ea412c51a438a238fd2d4802179855d99ffc86bd
Mathieu SIMON <mathieu.simon@inria.fr>
2019-02-28 10:46:10 +0100

    Get list of job ids by status

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/madlab"
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "MADLAB - WIP"
+
 
 @app.route('/job', methods=['POST'])
 def create_job():
@@ -20,20 +22,27 @@ def create_job():
     app = data.get('app', None)
     input_file = data.get('input', None)
     params = data.get('params', None)
-    _id = mongo.db.jobs.insert({'app': app, 'input': input_file, 'params': params})
+    _id = mongo.db.jobs.insert(
+        {'app': app, 'input': input_file, 'params': params})
     return str(_id)
+
 
 @app.route('/job/<id>')
 def get_job(id):
     return dumps(mongo.db.jobs.find_one_or_404({'_id': ObjectId(id)}))
+
 
 @app.route('/jobs/status', methods=['POST'])
 def get_jobs_status():
     data = request.get_json()
     if data is None:
         abort(500)
-    jobs = mongo.db.jobs.aggregate([{'$group' : {'_id':{'source':'$status'}, 'count':{'$sum':1}}} ])
+    jobs = mongo.db.jobs.aggregate(
+        [{'$group': {'_id': {'status': '$status'}, 'ids': {'$addToSet': {
+            '$toString': "$_id"
+        }}}}])
     return dumps(jobs)
+
 
 if __name__ == '__main__':
     app.run()
