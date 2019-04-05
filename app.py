1cea13e4410784271086de6baa2b73e813750892
Sébastien Campion <sebastien.campion@inria.fr>
2019-04-05 22:34:33 +0200

    swagger documentation

--- app.py
+++ app.py
@@ -51,21 +51,140 @@ async def jobs_status(request):
 
 # API
 ########################################################################################################################
-@routes.post('/batch')
-async def batch(request):
+@routes.get('/api/job/{id}', allow_head=False)
+async def get_job(request):
     """
     ---
-    description: Create to speed up batch submission and reduce http transfert.
-    Receive an http stream "Transfer-Encoding: chunked" of json.
-    tags:
-    - Batch scheduling
+    summary:  Retrieve job informations
+    parameters:
+        - in: path
+          name: id
+          schema:
+            type: hexstring
+          required: true
+          description: bson object ID of the job to get
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: a job in dictionary format
+    """
+    id = request.match_info.get('id')
+    data = mongo.madlab.jobs.find_one({'_id': ObjectId(id)})
+    if len(data) == 0:
+        web.Response(status=404)
+    else:
+        data['_id'] = id
+        return web.Response(body=dumps(data), content_type='application/json')
+
+
+@routes.post('/api/job')
+async def create_job(request):
+    """
+    ---
+    summary:  Create a job
+    parameters:
+        - in: body
+          name: data
+          description: The job to create.
+          schema:
+            type: object
+            required:
+              - app
+              - input
+            properties:
+              app:
+                type: string
+              input:
+                type: string
+              params:
+                type: object
+    produces:
+    - text/plain
+    responses:
+        "200":
+            description: a job identifier
+    """
+    data = await request.json()
+    if data is None:
+        web.Response(status=500)
+    del data['_id']
+    data['ctime'] = datetime.datetime.now()
+    log.debug("post job from %s for %s", request.transport.get_extra_info('peername'), data)
+    _id = mongo.madlab.jobs.insert(data)
+    return web.Response(text=str(_id))
+
+
+@routes.get('/api/jobs/status', allow_head=False)
+async def get_status(request):
+    """
+    ---
+    summary:  Retrieve counters of job per status
+    produces:
+    - application/json
+    responses:
+        '200':
+          description: a dictionary of status counter
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  status:
+                    type: string
+                    description: The job status.
+                  counter:
+                    type: integer
+                    description: The number of job in this job.
+    """
+    status_list = mongo.madlab.jobs.find().distinct('current_status')
+    status = {s: mongo.madlab.jobs.find({'current_status': s}).count() for s in status_list}
+    status['todo'] = status.pop(None) if None in status.keys() else 0
+    return web.json_response(status, headers={'Access-Control-Allow-Origin': "*"})
+
+
+@routes.get('/api/job/{id}/download/{path}', allow_head=False)
+async def download_job_file(request):
+    """
+    ---
+    summary:  Download a job file
+    description: job can produce file, this route enable to download it.
+    parameters:
+    - in: path
+      name: id
+      schema:
+        type: hexstring
+      required: true
+      description: bson object ID of the job to get
+    - in: path
+      name: path
+      schema:
+        type: string
+      required: true
+      description: path of the file
     produces:
     - application/octet-stream
     responses:
         "200":
-            description: response another http stream with object id bson encoded on 12 bytes when job is inserted
-        "405":
-            description: invalid HTTP Method
+          description: return the file
+    """
+    id = request.match_info.get('id')
+    path = request.match_info.get('path')
+    filepath = os.path.join(conf.jobs_dir, id, path)
+    return web.FileResponse(filepath)
+
+
+@routes.post('/api/batch')
+async def batch(request):
+    """
+    ---
+    summary:  Speed up batch submission
+    description: Reduce http transfert receive an http stream Transfer-Encoding chunked of json.
+    produces:
+    - application/octet-stream
+    responses:
+        "200":
+          description: response another http stream with object id bson encoded on 12 bytes when job is inserted
     """
     resp = web.StreamResponse(status=200, reason='OK', headers={'Content-Type': 'text/plain'})
     await resp.prepare(request)
@@ -83,8 +202,41 @@ async def batch(request):
     return resp
 
 
-@routes.get('/load')
+@routes.get('/api/load', allow_head=False)
 async def jobs_status(request):
+    """
+    ---
+    summary:  Retrieve load of cluster nodes
+    produces:
+    - application/json
+    responses:
+        '200':
+          description: a dictionary of status counter
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  host:
+                    type: string
+                    description: The host node.
+                  metrics:
+                    type: object
+                    description: mem, cpu, images, ...
+                    properties:
+                      mem:
+                        type: number
+                        description: memory load between 0 and 1
+                      cpu:
+                        type: number
+                        description: cpu load between 0 and 1
+                      images:
+                        type: object
+                        properties:
+                          image_name:
+                            type: integer
+                            description: number of container currently running
+    """
     def inspects(h):  # if container is removed during the request pass exception
         api = docker.APIClient(base_url=h, version=conf.docker_api_version)
         for c in api.containers():
@@ -106,14 +258,24 @@ async def jobs_status(request):
     return web.json_response(list(load_metrics()))
 
 
-@routes.get('/clean')
+@routes.get('/api/clean', allow_head=False)
 async def clean(request):
+    """
+    ---
+    summary:  Drop all jobs
+    """
     mongo.madlab.jobs.delete_many({})
-    return web.HTTPFound(location="/")
+    return web.Response(status=200)
 
 
-@routes.get('/status')
+@routes.get('/api/status', allow_head=False)
 async def status(request):
+    """
+    ---
+    summary:  Global status description
+    produces:
+    - application/json
+    """
     def containers():
         for d in dockers.values():
             yield {
@@ -140,57 +302,28 @@ async def status(request):
     return web.json_response(s)
 
 
-@routes.post('/job')
-async def create_job(request):
-    data = await request.json()
-    if data is None:
-        web.Response(status=500)
-    del data['_id']
-    data['ctime'] = datetime.datetime.now()
-    log.debug("post job from %s for %s", request.transport.get_extra_info('peername'), data)
-    _id = mongo.madlab.jobs.insert(data)
-    return web.Response(text=str(_id))
-
-
-@routes.get('/jobs/status')
-async def get_status(request):
-    status_list = mongo.madlab.jobs.find().distinct('current_status')
-    status = {s: mongo.madlab.jobs.find({'current_status': s}).count() for s in status_list}
-    status['todo'] = status.pop(None) if None in status.keys() else 0
-    return web.json_response(status, headers={'Access-Control-Allow-Origin': "*"})
-
-
-@routes.get('/job/{id}')
-async def get_job(request):
-    id = request.match_info.get('id')
-    data = mongo.madlab.jobs.find_one({'_id': ObjectId(id)})
-    if len(data) == 0:
-        web.Response(status=404)
-    else:
-        data['_id'] = id
-        return web.Response(body=dumps(data), content_type='application/json')
-
-
-@routes.get('/job/{id}/download/{path}')
-async def download_job_file(request):
-    id = request.match_info.get('id')
-    path = request.match_info.get('path')
-    filepath = os.path.join(conf.jobs_dir, id, path)
-    return web.FileResponse(filepath)
-
-
-@routes.get('/dataset.json')
+@routes.get('/api/dataset.json', allow_head=False)
 async def dataset(request):
+    """
+    ---
+    summary:  Retrieve dataset in order to backup it
+    produces:
+    - application/json
+    """
     return web.Response(body=dumps(mongo.madlab.dataset.find()), content_type='application/json')
 
 
 app = web.Application()
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates'))
-app.router.add_static('/assets/', path='static/assets', name='assets')
-app.router.add_static('/images/', path='static/images', name='images')
-app.router.add_static('/vendors/', path='static/vendors', name='vendors')
+for d in ['assets', 'images', 'vendors']:
+    app.router.add_static('/' + d, path=os.path.join('static', d), name=d)
 app.add_routes(routes)
-setup_swagger(app)
+
+setup_swagger(app,
+              description="Scalable cluster management and job scheduling system for large and small Docker clusters",
+              title="MadLab",
+              api_version="1.0",
+              contact="madlab@inria.fr")
 
 if __name__ == '__main__':
     web.run_app(app, port=5000)
