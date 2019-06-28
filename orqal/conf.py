7fbfa64d5d4ac3948ad7394a9814048f93b8cfad
Sebastien Campion <sebastien.campion@inria.fr>
2019-06-28 17:08:06 +0200

    Manage conf in DB

--- orqal/conf.py
+++ orqal/conf.py
@@ -1,23 +1,21 @@
-"""
-Conf using built-in data structures and dynamic loading
-
-"""
 import os
-import sys
+from pymongo import MongoClient
 
-fp = os.path.expanduser(os.path.join('~', '.orqal', 'conf.py'))
-if os.path.exists(fp):
-    sys.path.append(os.path.expanduser(os.path.join('~', '.orqal')))
-    from conf import *
-else:
-    mongourl = os.environ.get("ORQAL_MONGO_URL", "mongodb://localhost/orqal")
-    docker_hosts = ['192.168.100.%d:2376' % i for i in range(51, 65)]
-    mongo_replicaset = "madlabReplSet"
-    docker_api_version = '1.37'
-    registry_auth_config = {'username': 'test',
-                            'password': '65sX2-9sSXSp-hs-XeZ8'}
-    jobs_dir = "/scratch/jobs"
-    graphana_url = "http://localhost:3000/dashboard/db/docker-and-system-monitoring"
-    protected_containers = ['cadvisor']
-    nb_disp_jobs = 20
-    contact = "orqal@inria.fr"
+mongourl = os.getenv("ORQAL_MONGO_URI", 'mongodb://localhost/')
+mongo = MongoClient(mongourl)
+
+default = {'mongourl': 'mongodb://localhost/',
+           'mongo_replicaset': "madlabReplSet",
+           'docker_hosts': ['192.168.100.%d:2376' % i for i in range(51, 65)],
+           'docker_api_version': '1.37',
+           'registry_auth_config': {'username': 'test',
+                                    'password': '65sX2-9sSXSp-hs-XeZ8'},
+           'jobs_dir': "/scratch/jobs",
+           'nb_disp_jobs': 30,
+           'contact': "orqal@example.com",
+           }
+
+dbconf = mongo.orqal.conf.find_one({'active': True})
+mconf = {**default, **dbconf}
+locals().update(mconf)
+mongo.orqal.conf.replace_one({'_id': mconf['_id']}, mconf)
