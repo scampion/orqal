bafaf0e9e7ce3b248d7ac13580da9b0e9bc2ef10
Cassius Puodzius <cassius.puodzius@inria.com>
2019-07-12 09:46:47 +0200

    update

--- orqal/web.py
+++ orqal/web.py
@@ -461,7 +461,8 @@ async def status(request):
                    for ip, d in dockers.items()},
          "containers": [c for c in containers()]
          }
-    s["_services"].remove("AbstractWrapper")
+    if "AbstractWrapper" in s["_services"]:
+        s["_services"].remove("AbstractWrapper")
     return web.json_response(s)
 
 
